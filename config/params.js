ef956f38ce16a60671367ce044d55bbb30e262ce
mattlev97 <matteoenrico.levati@gmail.com>
2025-11-05 00:40:16 +0100

    Create params.js

---
+++ config/params.js
@@ -0,0 +1,10 @@
+/* /config/params.js
+   Configuration defaults - edit here before creating repo
+*/
+export const GITHUB_USERNAME = "your_github_username";
+export const REPO_NAME = "SplitTicket";
+
+export const DEFAULT_VOUCHER_VALUE_USER = 7.50;
+export const DEFAULT_VOUCHER_VALUE_PARTNER = 7.00;
+export const DEFAULT_VOUCHER_COUNT_USER = 1;
+export const DEFAULT_VOUCHER_COUNT_PARTNER = 1;
